af397850dca8719ee3a7ead0701c43cb51bd21a7
Emanuele 'Lele' Calo <emanuele.lele.calo@gmail.com>
2020-03-25 03:44:42 +0100

    Added update hook and script

---
+++ resources/update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+helm upgrade /var/lib/gravity/resources/charts/nginx-ingress                 \
+    --name gravity-ingress                                                   \
+    --namespace kube-system                                                  \
+    --set rbac.create=true                                                   \
+    --set podSecurityPolicy.enabled=true                                     \
+    --set controller.hostNetwork=true                                        \
+    --set controller.kind=DaemonSet                                          \
+    --set controller.daemonset.useHostPort=true
