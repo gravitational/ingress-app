5bc668081794f424b862854fc806c41de0305d95
Emanuele 'Lele' Calo <emanuele.lele.calo@gmail.com>
2020-03-25 03:46:16 +0100

    Removed left-over reference from storage-app

--- images/hook/entrypoint.sh
+++ images/hook/entrypoint.sh
@@ -14,17 +14,6 @@ if [ $1 = "update" ]; then
     echo "--> Starting upgrade, changeset: $RIG_CHANGESET"
     rig cs delete --force -c cs/${RIG_CHANGESET}
 
-    # TODO(r0mant): As this is the first release of OpenEBS as a Gravity application,
-    #               the current "upgrade" procedure assumes that OpenEBS is not
-    #               yet installed and thus just creates all OpenEBS resources
-    #               from scratch.
-    #               In the future, we'll need to develop a proper upgrade procedure
-    #               for OpenEBS components and its pools/volumes like described in
-    #               https://github.com/openebs/openebs/tree/master/k8s/upgrades.
-
-    echo "--> Creating OpenEBS resources"
-    rig upsert -f /var/lib/gravity/resources/openebs-operator.yaml --debug
-
     echo "--> Checking status"
     rig status ${RIG_CHANGESET} --retry-attempts=120 --retry-period=1s --debug
 
