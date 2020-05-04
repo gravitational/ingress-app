9405db9150d220178dfb91d03fc5a2ec468cbf2b
Emanuele 'Lele' Calo <emanuele.lele.calo@gmail.com>
2020-05-04 22:03:28 +0200

    Changed Helm to install to dedicated namespace

--- resources/install.sh
+++ resources/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 /usr/local/bin/helm install /var/lib/gravity/resources/charts/nginx-ingress  \
     --name gravity-ingress                                                   \
-    --namespace kube-system                                                  \
+    --namespace ingress                                                      \
     --set rbac.create=true                                                   \
     --set podSecurityPolicy.enabled=true                                     \
     --set controller.hostNetwork=true                                        \
